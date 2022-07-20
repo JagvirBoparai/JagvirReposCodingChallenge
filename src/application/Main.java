7ab98e0bc0ec38e42db91c933f24e4f5272db597
armaangrewal <armaangrewal@localhost>
2022-07-20 13:58:48 -0600

    Added my branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("Armaan's label"));
+			root.getChildren().add(new Label("Armaan's branch"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Jagvir's Project");
